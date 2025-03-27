1dd259472d65e0d79a3aaa4f102bf745a4ebe3d8
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-03-27 12:55:35 -0300

    second commit

---
+++ atividade-1/imc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+float peso;
+float altura; 
+printf("Digite seu peso: ");
+scanf("%f", &peso);
+printf("Digite sua altura: ");
+scanf("%f", &altura);
+float daltura = altura*altura;
+float imc = peso/daltura;
+printf("De acordo com o seu peso %.1f e a sua altura %.2f: seu IMC eh %.2f", peso, altura, imc);
+}
--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-    int idade = 30;
-    printf("A idade eh: %d", idade);
+    int idade;
+    float altura;
+    double peso; 
+    printf("Digite a idade: ");
+    scanf("%d", &idade);
+    printf("Digite a altura: ");
+    scanf("%f", &altura);
+    printf("Digite o peso: ");
+    scanf("%lf", &peso);
+    printf("A idade eh: %d\n", idade); 
+    printf("A altura eh: %.2fcm\nO peso eh: %.1lfkg\n", altura, peso);
+    double dobro_peso = peso*2.0;
+    printf("O dobro do peso eh: %.1lfkg.", dobro_peso);
     return 0;
 } 

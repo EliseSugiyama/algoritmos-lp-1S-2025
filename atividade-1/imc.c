60e8277f797046c9b9e22957331981b1143c2886
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-03-28 19:22:38 -0300

    second commit

--- atividade-1/imc.c
+++ atividade-1/imc.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
+
 float peso;
 float altura; 
-printf("Digite seu peso: ");
-scanf("%f", &peso);
-printf("Digite sua altura: ");
-scanf("%f", &altura);
+
+printf("Digite um peso: ");
+    scanf("%f", &peso);
+
+printf("Digite uma altura: ");
+    scanf("%f", &altura);
+
 float daltura = altura*altura;
 float imc = peso/daltura;
-printf("De acordo com o seu peso %.1f e a sua altura %.2f: seu IMC eh %.2f.", peso, altura, imc);
+
+printf("De acordo com o peso %.1f e a altura %.2f: o IMC eh %.2f.", peso, altura, imc);
 }

1ad81e36373078fde2c234c3ee87201969801762
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-03-28 19:24:54 -0300

    second commit

--- tipos_de_dados/numericos.c
+++ tipos_de_dados/numericos.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
+
     int idade;
     float altura;
     double peso; 
+
     printf("Digite a idade: ");
-    scanf("%d", &idade);
+        scanf("%d", &idade);
+
     printf("Digite a altura: ");
-    scanf("%f", &altura);
+        scanf("%f", &altura);
+
     printf("Digite o peso: ");
-    scanf("%lf", &peso);
+        scanf("%lf", &peso);
+
     printf("A idade eh: %d\n", idade); 
     printf("A altura eh: %.2fcm\nO peso eh: %.1lfkg\n", altura, peso);
+
     double dobro_peso = peso*2.0;
+
     printf("O dobro do peso eh: %.1lfkg.", dobro_peso);
+
     return 0;
 } 

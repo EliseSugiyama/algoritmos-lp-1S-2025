9d5b51f6c22ed2a764abde15d2e47c02b7dbba32
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-05-23 12:30:54 -0300

    Second commit

--- funcao/main.c
+++ funcao/main.c
@@ -7,26 +7,42 @@ typedef struct Aluno{
 } Aluno;
 
 void imprimir_aluno(Aluno a){
-    printf("\nNome: %s\nNota: %.2f\n", a.nome, a.nota);
-}
-
-void media(Aluno b){
+    printf("Nota: %.2f - %s\n", a.nota, a.nome);
 }
 
 int main(int argc, char* argv[]){
 
-    int n = 5;
-    struct Aluno alunos[n];
+    int n = 3;
+    Aluno alunos[n];
 
-    for(n = 0; n < 5; n++){
-    printf("\nDigite o nome do aluno: \n");
-    fgets(alunos[n].nome, TAM_NOME, stdin);
+    for(int i = 0; i < n; i++){
+    printf("\n\nDigite o nome do aluno %d: \n", i+1);
+    fgets(alunos[i].nome, TAM_NOME, stdin);
+    printf("\nDigite a nota do aluno %d: \n", i+1);
+    scanf("%f", &alunos[i].nota);
     fflush(stdin);
-    printf("Digite a nota do aluno: \n");
-    scanf("%f", &alunos[n].nota);
-    fflush(stdin);
-    
-    imprimir_aluno(alunos[n]);
+
     }
+
+    Aluno aluno_maior = alunos[0];
+    Aluno aluno_menor = alunos[0];
+    float media = 0.0, soma = 0.0;
+
+    for(int i = 0; i < n; i++){
+        if(aluno_maior.nota < alunos[i].nota){
+            aluno_maior.nota = alunos[i].nota;
+        }
+        if(aluno_menor.nota > alunos[i].nota){
+            aluno_menor.nota = alunos[i].nota;
+        }
+        soma += alunos[i].nota;
+    } 
+
+    printf("\n\nAluno maior nota: \n");
+    imprimir_aluno(aluno_maior);
+    printf("Aluno menor nota: \n");
+    imprimir_aluno(aluno_menor);
+    printf("Media das notas: %.2f\n\n", soma/n);
+
     return 0;
 }

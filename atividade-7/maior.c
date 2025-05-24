5c66aba7d2fb30b0700c55f9bebc75bd28140091
EliseSugiyama <elise.sugiyama5@gmail.com>
2025-05-24 19:07:58 -0300

    innitial commit

---
+++ atividade-7/maior.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+int main(int argc, char* argv[]){
+
+    int x, y, z;
+    int numeros [argc -1];
+
+    if(argc < 3){
+        printf("Informe 2 ou 3 argumentos.");
+    } else {
+        for(int i = 0; i < argc-1; i++){
+            numeros[i] = atoi(argv[i+1]);
+        }
+        printf("%d", maior_e(numeros, argc-1));
+    }
+    return 0;
+}
